85b379add32f3c56023b8739646e1a39a64a89ce
cg-dv <cgdevproject@gmail.com>
2021-09-04 16:54:22 -0700

    Fixed writeItem lambda function via AWS documentation example

--- js/writeItem.js
+++ js/writeItem.js
@@ -12,27 +12,81 @@
 const AWS = require('aws-sdk');
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = (event, context) => {
-    writeItem(); 
+exports.handler = (event, context, callback) => {
+    //if (!event.requestContext.authorizer) {
+      //errorResponse('Authorization not configured', context.awsRequestId, callback);
+      //return;
+    //}
+
+    // Because we're using a Cognito User Pools authorizer, all of the claims
+    // included in the authentication token are provided in the request context.
+    // This includes the username as well as other attributes.
+    //const username = event.requestContext.authorizer.claims['cognito:username'];
+
+    // The body field of the event in a proxy integration is a raw string.
+    // In order to extract meaningful values, we need to first parse this string
+    // into an object. A more robust implementation might inspect the Content-Type
+    // header first and use a different parsing strategy based on that value.
+    //const requestBody = JSON.parse(event.body);
+    var runId = "123456";
+    var date = "Sept-05-2021";
+    var distance = 3;
+    var runLocation = "Park";
+    var notes = "Notes about run";
+    writeItem(runId, date, distance, runLocation, notes).then(() => {
+        // You can use the callback function to provide a return value from your Node.js
+        // Lambda functions. The first parameter is used for failed invocations. The
+        // second parameter specifies the result data of the invocation.
+
+        // Because this Lambda function is called by an API Gateway proxy integration
+        // the result object must use the following structure.
+        callback(null, {
+            statusCode: 201,
+            body: JSON.stringify({
+                RunId: runId,
+                Date: date,
+                Distance: distance,
+                Location: runLocation,
+                Notes: notes,
+            }),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+    }).catch((err) => {
+        console.error(err);
+
+        // If there is an error during processing, catch it and return
+        // from the Lambda function successfully. Specify a 500 HTTP status
+        // code and provide an error message in the body. This will provide a
+        // more meaningful error response to the end client.
+        errorResponse(err.message, context.awsRequestId, callback)
+    });
+};
+
+function writeItem(runId, date, distance, runLocation, notes) {
+    return docClient.put({
+        TableName: 'Runs',
+        Item: {
+            RunId: runId,
+            Date: date,
+            Distance: distance,
+            Location: runLocation,
+            Notes: notes,
+        },
+    }).promise();
 }
 
-function writeItem() {
-    var params = {
-        TableName :"Runs",
-        Item:{
-            "RunID": "1234",
-            "Date": "Sept-5-2021",
-            "Distance": 4,
-            "Location": "Central Park",
-            "Notes": "Test Item"
-        }
-    };
-    docClient.put(params, function(err, data) {
-        if (err) {
-            return err 
-        } else {
-            return "\n" + JSON.stringify(data, undefined, 2)
-        }
-    });
+function errorResponse(errorMessage, awsRequestId, callback) {
+  callback(null, {
+    statusCode: 500,
+    body: JSON.stringify({
+      Error: errorMessage,
+      Reference: awsRequestId,
+    }),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
 }
 
