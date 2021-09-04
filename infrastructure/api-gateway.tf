46b4307d80985df2193309390c03817bc9023ad0
cg-dv <cgdevproject@gmail.com>
2021-09-04 07:03:39 -0700

    Added API Gateway resources, methods, and integrations for writeItem, listItem functions - temporarily disabled cognito authorizer for the purpose of testing basic functionality of read, write, and list functions

--- infrastructure/api-gateway.tf
+++ infrastructure/api-gateway.tf
@@ -5,32 +5,78 @@ resource "aws_api_gateway_rest_api" "run-log-api" {
   }
 }
 
-resource "aws_api_gateway_authorizer" "run-log-auth" {
-  name          = "run-log-auth"
-  type          = "COGNITO_USER_POOLS"
-  rest_api_id   = aws_api_gateway_rest_api.example.id
-  provider_arns = ["arn:aws:cognito-idp:us-east-1:414402433373:userpool/us-east-1_M1mWlsyXz"]
-}
+#resource "aws_api_gateway_authorizer" "run-log-auth" {
+#name          = "run-log-auth"
+#type          = "COGNITO_USER_POOLS"
+#rest_api_id   = aws_api_gateway_rest_api.example.id
+#provider_arns = ["arn:aws:cognito-idp:us-east-1:414402433373:userpool/us-east-1_M1mWlsyXz"]
+#}
 
 resource "aws_api_gateway_resource" "read" {
   rest_api_id = aws_api_gateway_rest_api.example.id
   parent_id   = aws_api_gateway_rest_api.example.root_resource_id
-  path_part   = "read"
+  path_part   = "readItem"
+}
+
+resource "aws_api_gateway_resource" "write" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  path_part   = "writeItem"
+}
+
+resource "aws_api_gateway_resource" "list" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  path_part   = "listItems"
+}
+
+resource "aws_api_gateway_method" "serverless-get-method" {
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  resource_id   = aws_api_gateway_resource.read.id
+  http_method   = "GET"
+  authorization = "NONE"
+  #authorizer_id = aws_api_gateway_authorizer.run-log-auth.id
 }
 
 resource "aws_api_gateway_method" "serverless-post-method" {
   rest_api_id   = aws_api_gateway_rest_api.example.id
-  resource_id   = aws_api_gateway_resource.read.id
+  resource_id   = aws_api_gateway_resource.write.id
   http_method   = "POST"
-  authorization = "COGNITO_USER_POOLS"
-  authorizer_id = aws_api_gateway_authorizer.run-log-auth.id
+  authorization = "NONE"
+  #authorizer_id = aws_api_gateway_authorizer.run-log-auth.id
 }
 
-resource "aws_api_gateway_integration" "example" {
+resource "aws_api_gateway_method" "serverless-get-method" {
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  resource_id   = aws_api_gateway_resource.list.id
+  http_method   = "GET"
+  authorization = "NONE"
+  #authorizer_id = aws_api_gateway_authorizer.run-log-auth.id
+}
+
+resource "aws_api_gateway_integration" "read-integration" {
+  http_method             = aws_api_gateway_method.serverless-get-method.http_method
+  integration_http_method = aws_api_gateway_method.serverless-get-method.http_method
+  uri                     = aws_lambda_function.readItem.invoke_arn
+  resource_id             = aws_api_gateway_resource.read.id
+  rest_api_id             = aws_api_gateway_rest_api.example.id
+  type                    = "AWS_PROXY"
+}
+
+resource "aws_api_gateway_integration" "write-integration" {
   http_method             = aws_api_gateway_method.serverless-post-method.http_method
   integration_http_method = aws_api_gateway_method.serverless-post-method.http_method
-  uri = aws_lambda_function.readItem.invoke_arn
-  resource_id             = aws_api_gateway_resource.read.id
+  uri                     = aws_lambda_function.writeItem.invoke_arn
+  resource_id             = aws_api_gateway_resource.write.id
+  rest_api_id             = aws_api_gateway_rest_api.example.id
+  type                    = "AWS_PROXY"
+}
+
+resource "aws_api_gateway_integration" "list-integration" {
+  http_method             = aws_api_gateway_method.serverless-get-method.http_method
+  integration_http_method = aws_api_gateway_method.serverless-get-method.http_method
+  uri                     = aws_lambda_function.listItems.invoke_arn
+  resource_id             = aws_api_gateway_resource.list.id
   rest_api_id             = aws_api_gateway_rest_api.example.id
   type                    = "AWS_PROXY"
 }
@@ -44,7 +90,7 @@ resource "aws_api_gateway_deployment" "example" {
 
   depends_on = [
     aws_api_gateway_rest_api.example,
-    aws_api_gateway_authorizer.run-log-auth,
+    #aws_api_gateway_authorizer.run-log-auth,
     aws_api_gateway_resource.read,
     aws_api_gateway_method.serverless-post-method,
     aws_api_gateway_integration.example
