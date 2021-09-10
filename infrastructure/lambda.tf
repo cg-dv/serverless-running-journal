1ff7573dd42611cb4d5ba6a564221e6ba35f5709
cg-dv <cgdevproject@gmail.com>
2021-09-10 07:29:38 -0700

    Consolidated Lambda functions into one Lambda function for all CRUD, scan operations

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -50,35 +50,15 @@ data "aws_s3_bucket" "lambda-code-bucket" {
   bucket = "lambda-serverless-example-app-code"
 }
 
-data "aws_s3_bucket_object" "readItem-function" {
+data "aws_s3_bucket_object" "CRUDList-function" {
   bucket = data.aws_s3_bucket.lambda-code-bucket.id
-  key    = "readItem.js.zip"
+  key    = "CRUDList.js.zip"
 }
 
-data "aws_s3_bucket_object" "writeItem-function" {
-  bucket = data.aws_s3_bucket.lambda-code-bucket.id
-  key    = "writeItem.js.zip"
-}
-
-data "aws_s3_bucket_object" "updateItem-function" {
-  bucket = data.aws_s3_bucket.lambda-code-bucket.id
-  key    = "updateItem.js.zip"
-}
-
-data "aws_s3_bucket_object" "deleteItem-function" {
-  bucket = data.aws_s3_bucket.lambda-code-bucket.id
-  key    = "deleteItem.js.zip"
-}
-
-#data "aws_s3_bucket_object" "listItems-function" {
-#bucket = data.aws_s3_bucket.lambda-code-bucket.id
-#key    = "listItems.js.zip"
-#}
-
-resource "aws_lambda_function" "readItem" {
-  function_name     = "readItem"
+resource "aws_lambda_function" "CRUD-list" {
+  function_name     = "CRUD-List"
   role              = aws_iam_role.lambda-execution-role-CRUD-query-scan.arn
-  handler           = "readItem.handler"
+  handler           = "CRUDList.handler"
   s3_bucket         = data.aws_s3_bucket.lambda-code-bucket.id
   s3_key            = data.aws_s3_bucket_object.readItem-function.key
   s3_object_version = null
@@ -86,50 +66,6 @@ resource "aws_lambda_function" "readItem" {
   runtime = "nodejs12.x"
 }
 
-resource "aws_lambda_function" "writeItem" {
-  function_name     = "writeItem"
-  role              = aws_iam_role.lambda-execution-role-dynamodb-write.arn
-  handler           = "writeItem.handler"
-  s3_bucket         = data.aws_s3_bucket.lambda-code-bucket.id
-  s3_key            = data.aws_s3_bucket_object.writeItem-function.key
-  s3_object_version = null
-
-  runtime = "nodejs12.x"
-}
-
-resource "aws_lambda_function" "updateItem" {
-  function_name     = "updateItem"
-  role              = aws_iam_role.lambda-execution-role-dynamodb-update.arn
-  handler           = "updateItem.handler"
-  s3_bucket         = data.aws_s3_bucket.lambda-code-bucket.id
-  s3_key            = data.aws_s3_bucket_object.updateItem-function.key
-  s3_object_version = null
-
-  runtime = "nodejs12.x"
-}
-
-resource "aws_lambda_function" "deleteItem" {
-  function_name     = "deleteItem"
-  role              = aws_iam_role.lambda-execution-role-dynamodb-delete.arn
-  handler           = "deleteItem.handler"
-  s3_bucket         = data.aws_s3_bucket.lambda-code-bucket.id
-  s3_key            = data.aws_s3_bucket_object.deleteItem-function.key
-  s3_object_version = null
-
-  runtime = "nodejs12.x"
-}
-
-#resource "aws_lambda_function" "listItems" {
-#function_name     = "listItems"
-#role              = aws_iam_role.lambda-execution-role-dynamodb-list.arn
-#handler           = "listItems.handler"
-#s3_bucket         = data.aws_s3_bucket.lambda-code-bucket.id
-#s3_key            = data.aws_s3_bucket_object.listItems-function.key
-#s3_object_version = null
-
-#runtime = "nodejs12.x"
-#}
-
 output "writeItem-Lambda-execution-role" {
-  value = aws_iam_role.lambda-execution-role-dynamodb-write.arn
+  value = aws_iam_role.lambda-execution-role-CRUD-query-scan.arn
 }
