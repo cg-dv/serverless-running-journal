66d7f4efc11e57d9cbce5ee0f22d2e3e096d4f21
cg-dv <cgdevproject@gmail.com>
2021-09-10 08:32:00 -0700

    Consolidated Lambda permission resources, API Gateway resources, and API Gateway Integrations to use one CRUD Lambda function rather than individual write, read, update, delete functions, resoureces, integrations

--- infrastructure/api-gateway.tf
+++ infrastructure/api-gateway.tf
@@ -7,55 +7,16 @@ resource "aws_api_gateway_rest_api" "run-log-api" {
   }
 }
 
-resource "aws_lambda_permission" "allow-api-gateway-read" {
+resource "aws_lambda_permission" "allow-api-gateway-CRUD-query-scan" {
   statement_id  = "AllowAPIInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.readItem.function_name
+  function_name = aws_lambda_function.CRUD-list.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.run-log-api.id}/*/*/*" 
+  source_arn    = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.run-log-api.id}/*/*/*"
 
   depends_on = [
     aws_api_gateway_rest_api.run-log-api,
-    aws_lambda_function.readItem
-  ]
-}
-
-resource "aws_lambda_permission" "allow-api-gateway-write" {
-  statement_id  = "AllowAPIInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.writeItem.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.run-log-api.id}/*/*/*" 
-
-  depends_on = [
-    aws_api_gateway_rest_api.run-log-api,
-    aws_lambda_function.writeItem
-  ]
-}
-
-resource "aws_lambda_permission" "allow-api-gateway-update" {
-  statement_id  = "AllowAPIInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.updateItem.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.run-log-api.id}/*/*/*" 
-
-  depends_on = [
-    aws_api_gateway_rest_api.run-log-api,
-    aws_lambda_function.updateItem
-  ]
-}
-
-resource "aws_lambda_permission" "allow-api-gateway-delete" {
-  statement_id  = "AllowAPIInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.deleteItem.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.run-log-api.id}/*/*/*" 
-
-  depends_on = [
-    aws_api_gateway_rest_api.run-log-api,
-    aws_lambda_function.deleteItem
+    aws_lambda_function.CRUD-list
   ]
 }
 
@@ -66,120 +27,28 @@ resource "aws_lambda_permission" "allow-api-gateway-delete" {
 #provider_arns = ["arn:aws:cognito-idp:us-east-1:414402433373:userpool/us-east-1_M1mWlsyXz"]
 #}
 
-resource "aws_api_gateway_resource" "read" {
+resource "aws_api_gateway_resource" "any" {
   rest_api_id = aws_api_gateway_rest_api.run-log-api.id
   parent_id   = aws_api_gateway_rest_api.run-log-api.root_resource_id
-  path_part   = "readItem"
-}
-
-resource "aws_api_gateway_resource" "write" {
-  rest_api_id = aws_api_gateway_rest_api.run-log-api.id
-  parent_id   = aws_api_gateway_rest_api.run-log-api.root_resource_id
-  path_part   = "writeItem"
-}
-
-resource "aws_api_gateway_resource" "update" {
-  rest_api_id = aws_api_gateway_rest_api.run-log-api.id
-  parent_id   = aws_api_gateway_rest_api.run-log-api.root_resource_id
-  path_part   = "updateItem"
-}
-
-resource "aws_api_gateway_resource" "delete" {
-  rest_api_id = aws_api_gateway_rest_api.run-log-api.id
-  parent_id   = aws_api_gateway_rest_api.run-log-api.root_resource_id
-  path_part   = "deleteItem"
-}
-
-#resource "aws_api_gateway_resource" "list" {
-  #rest_api_id = aws_api_gateway_rest_api.run-log-api.id
-  #parent_id   = aws_api_gateway_rest_api.run-log-api.root_resource_id
-  #path_part   = "listItems"
-#}
-
-resource "aws_api_gateway_method" "serverless-get-method" {
-  rest_api_id   = aws_api_gateway_rest_api.run-log-api.id
-  resource_id   = aws_api_gateway_resource.read.id
-  http_method   = "GET"
-  authorization = "NONE"
-  #authorizer_id = aws_api_gateway_authorizer.run-log-auth.id
+  path_part   = "CRUDList"
 }
 
 resource "aws_api_gateway_method" "serverless-post-method" {
   rest_api_id   = aws_api_gateway_rest_api.run-log-api.id
-  resource_id   = aws_api_gateway_resource.write.id
+  resource_id   = aws_api_gateway_resource.any.id
   http_method   = "POST"
   authorization = "NONE"
   #authorizer_id = aws_api_gateway_authorizer.run-log-auth.id
 }
 
-resource "aws_api_gateway_method" "serverless-post-method-update" {
-  rest_api_id   = aws_api_gateway_rest_api.run-log-api.id
-  resource_id   = aws_api_gateway_resource.update.id
-  http_method   = "POST"
-  authorization = "NONE"
-  #authorizer_id = aws_api_gateway_authorizer.run-log-auth.id
-}
-
-resource "aws_api_gateway_method" "serverless-post-method-delete" {
-  rest_api_id   = aws_api_gateway_rest_api.run-log-api.id
-  resource_id   = aws_api_gateway_resource.delete.id
-  http_method   = "POST"
-  authorization = "NONE"
-  #authorizer_id = aws_api_gateway_authorizer.run-log-auth.id
-}
-
-#resource "aws_api_gateway_method" "serverless-get-method-list" {
-#rest_api_id   = aws_api_gateway_rest_api.run-log-api.id
-#resource_id   = aws_api_gateway_resource.list.id
-#http_method   = "GET"
-#authorization = "NONE"
-#authorizer_id = aws_api_gateway_authorizer.run-log-auth.id
-#}
-
-resource "aws_api_gateway_integration" "read-integration" {
-  http_method             = aws_api_gateway_method.serverless-get-method.http_method
-  integration_http_method = aws_api_gateway_method.serverless-get-method.http_method
-  uri                     = aws_lambda_function.readItem.invoke_arn
-  resource_id             = aws_api_gateway_resource.read.id
-  rest_api_id             = aws_api_gateway_rest_api.run-log-api.id
-  type                    = "AWS_PROXY"
-}
-
-resource "aws_api_gateway_integration" "write-integration" {
+resource "aws_api_gateway_integration" "CRUD-integration" {
   http_method             = aws_api_gateway_method.serverless-post-method.http_method
   integration_http_method = aws_api_gateway_method.serverless-post-method.http_method
-  uri                     = aws_lambda_function.writeItem.invoke_arn
-  resource_id             = aws_api_gateway_resource.write.id
+  uri                     = aws_lambda_function.CRUD-list.invoke_arn
+  resource_id             = aws_api_gateway_resource.any.id
   rest_api_id             = aws_api_gateway_rest_api.run-log-api.id
   type                    = "AWS_PROXY"
 }
-
-resource "aws_api_gateway_integration" "update-integration" {
-  http_method             = aws_api_gateway_method.serverless-post-method-update.http_method
-  integration_http_method = aws_api_gateway_method.serverless-post-method-update.http_method
-  uri                     = aws_lambda_function.updateItem.invoke_arn
-  resource_id             = aws_api_gateway_resource.update.id
-  rest_api_id             = aws_api_gateway_rest_api.run-log-api.id
-  type                    = "AWS_PROXY"
-}
-
-resource "aws_api_gateway_integration" "delete-integration" {
-  http_method             = aws_api_gateway_method.serverless-post-method-delete.http_method
-  integration_http_method = aws_api_gateway_method.serverless-post-method-delete.http_method
-  uri                     = aws_lambda_function.deleteItem.invoke_arn
-  resource_id             = aws_api_gateway_resource.delete.id
-  rest_api_id             = aws_api_gateway_rest_api.run-log-api.id
-  type                    = "AWS_PROXY"
-}
-
-#resource "aws_api_gateway_integration" "list-integration" {
-#http_method             = aws_api_gateway_method.serverless-get-method-list.http_method
-#integration_http_method = aws_api_gateway_method.serverless-get-method-list.http_method
-#uri                     = aws_lambda_function.listItems.invoke_arn
-#resource_id             = aws_api_gateway_resource.list.id
-#rest_api_id             = aws_api_gateway_rest_api.run-log-api.id
-#type                    = "AWS_PROXY"
-#}
 
 resource "aws_api_gateway_deployment" "example" {
   rest_api_id = aws_api_gateway_rest_api.run-log-api.id
@@ -189,12 +58,12 @@ resource "aws_api_gateway_deployment" "example" {
   }
 
   depends_on = [
-    aws_lambda_function.writeItem,
+    aws_lambda_function.CRUD-list,
     aws_api_gateway_rest_api.run-log-api,
     #aws_api_gateway_authorizer.run-log-auth,
-    aws_api_gateway_resource.write,
+    aws_api_gateway_resource.any,
     aws_api_gateway_method.serverless-post-method,
-    aws_api_gateway_integration.write-integration
+    aws_api_gateway_integration.CRUD-integration
   ]
 }
 
